f6106cb4d6019836f255927c2d3f5a204ef53b5c
Martin Seyferth <martin.seyferth@gmail.com>
2022-03-12 09:59:33 +0100

    Add projection

---
+++ src/alpha_proj.m
@@ -0,0 +1,39 @@
+function [alpha_k] = alpha_proj(alpha_tilde, alpha_up, alpha_down)
+%ALPHA_PROJ Summary of this function goes here
+%   Detailed explanation goes here
+eps_alpha = 1e-12;
+tol_p = 1e-4;
+size_ = numel(alpha_tilde);
+alpha_k = ones(size_, 1)  * alpha_down;
+laplace_n_ = laplace_n(sqrt(size_), sqrt(size_));
+
+
+res = @(alpha) (speye(size_) - laplace_n_)*(alpha - alpha_tilde) + 1 / eps_alpha * (arrayfun(@(v)smooth_max(v, 0), alpha - alpha_up) - arrayfun(@(v)smooth_max(v, 0), alpha_down -  alpha));
+h1_dual = @(v) 1 / sqrt(size_) * sqrt(v' * ((speye(size_) - laplace_n_) \ v));
+
+res_0 = res(alpha_k);
+
+while true
+    res_k = res(alpha_k);
+    xhi_l = arrayfun(@(v)v > alpha_up ||  v  <  alpha_down, alpha_k);
+    delta_alpha = (speye(size_) - laplace_n_ + 1 / eps_alpha * spdiags(xhi_l,0, size_, size_)) \ -res_k;
+    
+    alpha_k = alpha_k + delta_alpha;
+    
+    if h1_dual(res_k) < tol_p * h1_dual(res_0)
+        break
+    end
+end
+
+function y = smooth_max(r, delta)
+    if r >= delta
+        y = r - delta / 2;
+    elseif r > 0 && r < delta
+        y = 0.5 * r^2 / delta;
+    else
+        y = 0;
+    end
+end
+
+end
+
